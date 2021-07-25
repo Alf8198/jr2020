63f2e618f791a9098fbb9bfb2ffe01e15264e531
Sergey Leuhin <31506558+Alf8198@users.noreply.github.com>
2021-07-25 17:36:26 +0300

    Add files via upload

---
+++ task/task12/task1208/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int a){}
+    public static void print(long a){}
+    public static void print(float a){}
+    public static void print(byte a){}
+    public static void print(Integer a){}
+
+    //Напишите тут ваши методы
+}
